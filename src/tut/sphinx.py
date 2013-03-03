20c3919e774cabf8708717fc8f034af2fb6e09d3
Nathan Yergler <nathan@yergler.net>
2013-03-03 11:59:16 -0800

    Cleanup when switching tags and when finishing the build.
    * Clear the sphinx.pycode cache when a checkpoint is encountered; this
      prevents sphinx from showing an out of date code sample in a
      literalinclude block.
    * Record the current state the first time a tut path is encountered,
      and check out that state when the build finishes.

--- src/tut/sphinx.py
+++ src/tut/sphinx.py
@@ -1,10 +1,13 @@
+from __future__ import absolute_import
 import os
 
 from sh import git
 from docutils.parsers.rst import Directive, directives
+import sphinx.pycode
 
 
 _DEFAULT_PATH = None
+_RESET_PATHS = {}
 
 
 class TutDefaults(Directive):
@@ -33,6 +36,7 @@ class TutCheckpoint(Directive):
     def run(self):
 
         global _DEFAULT_PATH
+        global _RESET_PATHS
 
         if 'path' in self.options:
             tut_path = self.options['path']
@@ -41,18 +45,40 @@ class TutCheckpoint(Directive):
         else:
             raise Exception("No tut path specified.")
 
+        # paths are relative to the project root
+        if tut_path[0] == '/':
+            tut_path = tut_path[1:]
+
         curdir = os.getcwd()
         tut_path = os.path.join(curdir, tut_path)
         os.chdir(tut_path)
 
+        # if this is the first time visiting this repo
+        if tut_path not in _RESET_PATHS:
+            # record the current branch
+            _RESET_PATHS[tut_path] = \
+                git('name-rev', 'HEAD').strip().split()[-1]
+
         git.checkout(self.arguments[0].strip().lower())
+        sphinx.pycode.ModuleAnalyzer.cache = {}
 
         os.chdir(curdir)
 
         return []
 
 
+def cleanup(app, exception):
+
+    global __RESET_PATHS
+
+    for path in _RESET_PATHS:
+        os.chdir(path)
+        git.checkout(_RESET_PATHS[path])
+
+
 def setup(app):
 
     app.add_directive('tut', TutDefaults)
     app.add_directive('checkpoint', TutCheckpoint)
+
+    app.connect('build-finished', cleanup)
