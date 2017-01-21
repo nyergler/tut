0e8d14a529c3006c787a4c63576740ae43401458
Nathan Yergler <nathan@yergler.net>
2017-01-21 15:01:39 -0800

    Atempt to order doc processing in TOC order.

--- src/tut/sphinx/__init__.py
+++ src/tut/sphinx/__init__.py
@@ -1,3 +1,5 @@
+from docutils.nodes import reference
+
 from tut import version
 from tut.sphinx.checkpoint import (
     TutDefaults,
@@ -5,6 +7,51 @@ from tut.sphinx.checkpoint import (
     initialize,
     cleanup,
 )
+from tut.sphinx.content import (
+    TutContent,
+    TutExec,
+)
+
+
+def order_docs(app, env, docnames):
+    """Force Sphinx to process documents in TOC order."""
+
+    # TK: To get the complete ordering this will need to descend into the
+    # referenced documents and inspect their toctrees
+
+    try:
+        toc = env.get_toctree_for(app.config.master_doc, app.builder, False)
+    except FileNotFoundError:
+        app.warn('No toctree available; consider re-building')
+        toc = None
+
+    if toc is None:
+        return
+
+    entries = toc.traverse(descend=True, condition=reference)
+
+    remaining_docnames = env.found_docs.copy() - {app.config.master_doc}
+    docnames.clear()
+    docnames.append(app.config.master_doc)
+
+    for entry in entries:
+        if not entry.attributes.get('internal'):
+            # not an internal reference
+            continue
+
+        docname = entry.attributes['refuri'].rsplit('#', 1)[0].rsplit('.', 1)[0]
+        if docname in remaining_docnames:
+            docnames.append(docname)
+            remaining_docnames.remove(docname)
+
+    if remaining_docnames:
+        # there are documents not in the tree, emit a warning
+        app.warn(
+            'Some documents are not able to be explicitly ordered; consider re-building.',
+            remaining_docnames,
+        )
+
+        docnames.extend(list(remaining_docnames))
 
 
 def setup(app):
@@ -14,6 +61,7 @@ def setup(app):
 
     app.connect('builder-inited', initialize)
     app.connect('build-finished', cleanup)
+    app.connect('env-before-read-docs', order_docs)
 
     return {
         'version': version(),
