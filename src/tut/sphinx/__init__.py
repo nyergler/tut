a92a66f01ac1b478b4b4aaed665fcce7f4d584c5
Nathan Yergler <nathan@yergler.net>
2017-04-19 11:10:07 -0700

    Remove attempt at absolute doc ordering.

--- src/tut/sphinx/__init__.py
+++ src/tut/sphinx/__init__.py
@@ -17,47 +17,6 @@ from tut.sphinx.code import (
 )
 
 
-def order_docs(app, env, docnames):
-    """Force Sphinx to process documents in TOC order."""
-
-    # TK: To get the complete ordering this will need to descend into the
-    # referenced documents and inspect their toctrees
-
-    try:
-        toc = env.get_toctree_for(app.config.master_doc, app.builder, False)
-    except FileNotFoundError:
-        app.warn('No toctree available; consider re-building')
-        toc = None
-
-    if toc is None:
-        return
-
-    entries = toc.traverse(descend=True, condition=reference)
-
-    remaining_docnames = env.found_docs.copy() - {app.config.master_doc}
-    docnames.clear()
-    docnames.append(app.config.master_doc)
-
-    for entry in entries:
-        if not entry.attributes.get('internal'):
-            # not an internal reference
-            continue
-
-        docname = entry.attributes['refuri'].rsplit('#', 1)[0].rsplit('.', 1)[0]
-        if docname in remaining_docnames:
-            docnames.append(docname)
-            remaining_docnames.remove(docname)
-
-    if remaining_docnames:
-        # there are documents not in the tree, emit a warning
-        app.warn(
-            'Some documents are not able to be explicitly ordered; consider re-building.',
-            remaining_docnames,
-        )
-
-        docnames.extend(list(remaining_docnames))
-
-
 def setup(app):
 
     app.add_directive('tut', TutDefaults)
@@ -70,7 +29,6 @@ def setup(app):
 
     app.connect('builder-inited', initialize)
     app.connect('build-finished', cleanup)
-    app.connect('env-before-read-docs', order_docs)
 
     return {
         'version': version(),
